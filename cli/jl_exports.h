aa166f3d9eca43124d85ed76f3f001de16f2c881
Jameson Nash <vtjnash@gmail.com>
2021-09-02 16:29:45 -0400

    use separate names for internal and exported functions
    helps avoid linker errors and confusion

--- cli/jl_exports.h
+++ cli/jl_exports.h
@@ -21,13 +21,13 @@ typedef void (anonfunc)(void);
 JL_EXPORTED_FUNCS(XX)
 #undef XX
 
-// Define holder locations for function addresses as `const void * $(name)_addr = & $(name);`
-#define XX(name)    JL_HIDDEN anonfunc * name##_addr = (anonfunc*)&name;
+// Define holder locations for function addresses as `const void * $(name)_addr = NULL;
+#define XX(name)    JL_HIDDEN anonfunc * name##_addr = NULL;
 JL_EXPORTED_FUNCS(XX)
 #undef XX
 
 // Generate lists of function names and addresses
-#define XX(name)    #name,
+#define XX(name)    "i" #name,
 static const char *const jl_exported_func_names[] = {
     JL_EXPORTED_FUNCS(XX)
     NULL
